e8eb4937856d7c861c41ee8ec921ffd88c68d69a
iamshivam1 <martinsingh9001@gmail.com>
2024-08-31 08:00:37 +0000

    variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("shivam")
+const accountId = 145553
+let accountEmail = "shivam@gmail.com"
+var accountPass = "123"
+accountCity = "Jaipur"
+
+// accountId = 2
+console.log(accountId);
+accountEmail = "shivam123@gmail.com"
+accountPass = "12345"
+accountCity = "Jammu"
+console.table([accountId,accountEmail,accountPass,accountCity]);
+
+let accountState;
