2a3233c68c01e2070c02e3d2f60e55110295e8b8
iamshivam1 <martinsingh9001@gmail.com>
2024-09-09 08:49:48 +0000

    conversion

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,22 @@
+// let score = null
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1;
+
+// let isLoggedIn = ''
+// let bolleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(bolleanIsLoggedIn);
+// // 1 => true ; 0 => false
+// // '' => false 
+// // "shivam" => true 
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
