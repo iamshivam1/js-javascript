270181087383a88ad75c54c6d63e086cf037ef3b
iamshivam1 <martinsingh9001@gmail.com>
2024-09-10 13:41:44 +0000

    datatypes

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,33 @@
+// Primitive datatypes
+
+// 7 types : string ,number, bullean, null, undefined, symbol , Bigint
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 36367378383883n
+
+// Refrence type (Non Primitive datatypes )
+
+// Array,Objects,Functions
+
+const heroes = ["shaktiman","krish","Vikral"]
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+
+
