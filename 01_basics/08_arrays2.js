87e6739a45c7d769c61d74ab2d159275963228fc
iamshivam1 <martinsingh9001@gmail.com>
2024-09-11 12:49:24 +0000

    arrays2

---
+++ 01_basics/08_arrays2.js
@@ -0,0 +1,10 @@
+const marvel_heroes = ["thor", "Ironmman", "Spiderman"]
+const dc_heroes = ["Superman", "flash", "Batman"]
+
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+let mixHeroes = marvel_heroes.concat(dc_heroes)
+console.log(mixHeroes);
+
