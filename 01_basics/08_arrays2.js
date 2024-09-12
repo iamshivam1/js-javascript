f2c201b7cd66bb618ca511db173222dea041620a
iamshivam1 <martinsingh9001@gmail.com>
2024-09-12 07:28:44 +0000

    arrays2

--- 01_basics/08_arrays2.js
+++ 01_basics/08_arrays2.js
@@ -5,6 +5,22 @@ const dc_heroes = ["Superman", "flash", "Batman"]
 // console.log(marvel_heroes);
 // console.log(marvel_heroes[3][1]);
 
-let mixHeroes = marvel_heroes.concat(dc_heroes)
-console.log(mixHeroes);
+// let mixHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(mixHeroes);
 
+const all_new_arrays = [...marvel_heroes, ...dc_heroes]
+// console.log(all_new_arrays);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const usable_another_array = another_array.flat(Infinity)
+// console.log(usable_another_array);
+
+// console.log(Array.isArray("Shivam"))
+// console.log(Array.from("Shivam"))
+// console.log(Array.from({name: "Shivam"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
