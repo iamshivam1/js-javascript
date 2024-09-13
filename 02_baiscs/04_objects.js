24ebdd7803a08eeb8250f124261a5332b55da792
iamshivam1 <martinsingh9001@gmail.com>
2024-09-13 10:41:11 +0000

    objects

--- 02_baiscs/04_objects.js
+++ 02_baiscs/04_objects.js
@@ -1,3 +1,13 @@
-const tinder = new Object() // singleton object
-cost tinder_user = {}
-console.log(tinder);
+// const tinder = new Object() // singleton object
+// cost tinder_user = {}
+// // console.log(tinder);
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Shivam"
+}
+
+// course.courseInstructor
+const {courseInstructor} = course
+console.log(courseInstructor);
