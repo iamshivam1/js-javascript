247750d950de80bd6404eb9c388def58f51245fe
iamshivam1 <martinsingh9001@gmail.com>
2024-09-17 16:24:06 +0000

    arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,16 +1,46 @@
-const user = {
-   username: "Shivam",
-   price: 999,
+// const user = {
+//    username: "Shivam",
+//    price: 999,
 
-   welcomeMessage: function(){
-    console.log(`${this.username} ,welcome to website`);
+//    welcomeMessage: function() {
+//     console.log(`${this.username} ,welcome to website`);
     
-   }
+//    }
+// }
 
 
-}
 
-user.welcomeMessage()
-// user.price()
+
+// user.welcomeMessage()
+// // user.price()
 // user.username = "Shivam Gupta"
-// console.log(user.username())
+// // console.log(user.username())
+// user.welcomeMessage()
+
+// function chai(){
+//    let username = "Shivam"
+//    console.log(this);
+   
+// }
+
+// chai()
+
+// const chai = () => {
+//  let username = "Harry" 
+//    console.log(this.username);
+   
+// }
+// chai()
+
+// const addTwo = (num1,num2) => {
+//    return num1 + num2
+// }
+
+// console.log(addTwo(2, 3))
+
+// const addTwo = (num1,num2) => num1 + num2
+// const addTwo = (num1,num2) => (num1 + num2)
+// console.log(addTwo(2, 3))
+
+const addTwostr = (num1,num2) => ({username: "Shivam"})
+console.log(addTwostr(2, 3))
