5c423e9fba27135f39b73dbaa17c88f183fbfdb1
iamshivam1 <martinsingh9001@gmail.com>
2024-09-17 17:50:11 +0000

    iffe

---
+++ 03_basics/04_IFFIE.js
@@ -0,0 +1,7 @@
+// Immediately Invoked Function Expression
+(function chai() {
+    console.log("DB CONNECTED");
+})();
+
+// jo function Immediately exicute ho jaye aur global function k pollution sy bachne k liye bhi 
+// iffe k baad semicolon nhi wo chlta jayega
